b16800f55dd8caa87ce237eb03a1ad4e70b6147f
sn77sohan <sohan.ahmedself@gmail.com>
2021-10-25 22:57:04 +0600

    rest-countries

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// https://restcountries.com/v2/all
+
+// const loadCountries = () => {
+//     fetch('https://restcountries.com/v2/all')
+//     .then(res =>res.json())
+//     .then(data => displayCountries(data));
+// }
+// const displayCountries = countries =>{
+//     console.log(countries);
+// }
+// loadCountries();
+
+const loadCountries = () => {
+    fetch('https://restcountries.com/v2/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data));
+}
+
+
+const displayCountries = countries => {
+    console.log(countries[0]);
+    const container = document.getElementById('countries');
+    const countryHTML= countries.map(country=>getCountryHTML(country));
+    container.innerHTML = countryHTML.join([]);
+    console.log(countryHTML[0]);
+}
+const getCountryHTML = country =>{
+    return `
+    <div class="country">
+        <h2>${country.name}</h2>
+        <h4>${country.capital}</h4>
+        <h4>${country.timezones}</h4>
+        <h4>${country.capital}</h4>
+        <img src="${country.flag}">
+    </div>
+    `
+}
+loadCountries();
